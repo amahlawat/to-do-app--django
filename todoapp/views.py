efae776738c4b2cc94a4c9f61f5a1b9d9d8774f5
amahlawat <avinash532110@gmail.com>
2022-05-08 11:11:49 +0530

    Feat: adding update todoItem

--- todoapp/views.py
+++ todoapp/views.py
@@ -12,7 +12,7 @@ def todoappView(request):
 def addTodoView(request):
     x = request.POST.get('content', False)
     new_item = TodoListItem(content = x)
-    print(" new_item ", new_item)
+    print(" TodoListItem ", TodoListItem)
     new_item.save()
     return HttpResponseRedirect('/todoapp/')
 
@@ -24,4 +24,9 @@ def deleteTodoView(request, i):
 def editView(request, i):
     y = TodoListItem.objects.get(id=i)
     return render(request, 'edit.html',
-    {'data': y}) 
+    {'data': { 'idx': 'test1', 'value': y}}) 
+
+def updateTodoItem(request, i):
+    TodoListItem.objects.filter(pk=i).update(field1='some value')
+    return HttpResponseRedirect('/todoapp/')
+    
