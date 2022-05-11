df09fd5a9490aaa20c8decd84b3c1e70ebe9edf1
amahlawat <avinash532110@gmail.com>
2022-05-11 12:10:46 +0530

    Fix: issue db add new fields

--- todoapp/models.py
+++ todoapp/models.py
@@ -2,5 +2,7 @@ from django.db import models
 
 # Create your models here.
 
-class TodoListItem(models.Model):
-    content = models.TextField()
+class TodoListItem2(models.Model):
+    description = models.TextField()
+    status = models.TextField()
+
