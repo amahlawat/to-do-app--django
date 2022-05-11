cc6153f5ce777c49e14acaa0631f3c9109b4bb93
amahlawat <avinash532110@gmail.com>
2022-05-11 12:10:51 +0530

    Fix: issue db add new fields

--- todoapp/views.py
+++ todoapp/views.py
@@ -1,35 +1,35 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
-from .models import TodoListItem
+from .models import TodoListItem2
 from django.http import HttpResponseRedirect
 
 # Create your views here.
 def todoappView(request):
-    all_todo_items = TodoListItem.objects.all()
+    all_todo_items = TodoListItem2.objects.all()
     return render(request, 'todolist.html',
     {'all_items': all_todo_items}) 
 
 def addTodoView(request):
     x = request.POST.get('content', False)
-    new_item = TodoListItem(description = x, status = 'inprogress')
+    new_item = TodoListItem2(description = x, status = 'in progress')
     new_item.save()
     return HttpResponseRedirect('/todoapp/')
 
 def deleteTodoView(request, i):
-    y = TodoListItem.objects.get(id=i)
+    y = TodoListItem2.objects.get(id=i)
     y.delete()
     return HttpResponseRedirect('/todoapp/')
 
 def editView(request, i):
-    y = TodoListItem.objects.get(id=i)
+    y = TodoListItem2.objects.get(id=i)
     return render(request, 'edit.html',
     {'data': { 'idx': i, 'value': y}}) 
 
 def updateTodoItem(request, i):
     updatedData = request.POST.get('updatedData', False)
-    TodoListItem.objects.filter(pk=i).update(content = updatedData)
+    TodoListItem2.objects.filter(pk=i).update(content = updatedData)
     return HttpResponseRedirect('/todoapp/')
     
 # def taskStatus(request, i):
-#     TodoListItem.objects.filter(pk=i).update(status = updatedData)
+#     TodoListItem2.objects.filter(pk=i).update(status = updatedData)
 #     return HttpResponseRedirect('/todoapp/')
